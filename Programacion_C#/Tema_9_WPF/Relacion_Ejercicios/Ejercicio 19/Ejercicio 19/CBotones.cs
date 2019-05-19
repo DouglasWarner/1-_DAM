0a6eb29ddcba2c8819b06bb17a98c60853606f4f
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-05-19 20:21:18 +0200

    Ejercicio 19

---
+++ Programacion_C#/Tema_9_WPF/Relacion_Ejercicios/Ejercicio 19/Ejercicio 19/CBotones.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Ejercicio_19
+{
+	class CBotones
+	{
+		Button btn;
+
+		public Button Btn { get => btn; set => btn = value; }
+
+		public CBotones(int i, int j)
+		{
+			Btn = new Button();
+			Btn.Content = string.Format("[{0},{1}]",i,j);
+			Btn.FontSize = 20;
+			Btn.Height = 30;
+			Btn.Width = 50;
+			btn.Background = Brushes.DarkCyan;
+			Btn.Margin = new Thickness(10,10,10,10);
+			//btn.MouseMove += Btn_MouseMove;
+			Btn.MouseLeave += Btn_MouseLeave;
+			Btn.Click += Btn_Click;
+			//btn.PreviewMouseMove += Btn_PreviewMouseMove;
+			//btn.PreviewMouseUp += Btn_PreviewMouseUp;
+		}
+
+		private void Btn_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			this.Btn.Background = Brushes.DarkCyan;
+		}
+
+		private void Btn_PreviewMouseMove(object sender, MouseEventArgs e)
+		{
+			this.Btn.Background = Brushes.DarkCyan;
+		}
+
+		private void Btn_MouseMove(object sender, MouseEventArgs e)
+		{
+			this.Btn.Background = Brushes.DarkCyan;
+		}
+
+		private void Btn_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(string.Format("Hola soy el boton {0}", Btn.Content));
+		}
+
+		private void Btn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			if (this.btn.Background == Brushes.Red)
+				this.btn.Background = Brushes.DarkBlue;
+			else
+				this.Btn.Background = Brushes.Red;
+		}
+	}
+}
---
+++ Programacion_C#/Tema_9_WPF/Relacion_Ejercicios/Ejercicio 19/Ejercicio 19/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_19
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		int fila;
+		int columna;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Inicializar();
+		}
+
+		void Inicializar()
+		{
+			for (int i = 1; i < 21; i++)
+			{
+				cbxFilas.Items.Add(i);
+				cbxColumnas.Items.Add(i);
+			}
+			cbxFilas.SelectedItem = cbxFilas.Items[5];
+			cbxColumnas.SelectedIndex = 5;
+		}
+
+		private void BtnCrearMatriz_Click(object sender, RoutedEventArgs e)
+		{
+			stpMatriz.Children.Clear();
+			fila = int.Parse(cbxFilas.SelectedItem.ToString());
+			columna = int.Parse(cbxColumnas.SelectedItem.ToString());
+			CBotones btn;
+
+			for (int i = 0; i < fila; i++)
+			{
+				for (int j = 0; j < columna; j++)
+				{
+					btn = new CBotones(i, j);
+					stpMatriz.Children.Add(btn.Btn);
+				}				
+			}
+		}
+		
+	}
+}
