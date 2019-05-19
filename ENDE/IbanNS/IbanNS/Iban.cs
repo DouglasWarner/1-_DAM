7a37e5545146e586264201de638769e374be1b30
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-05-19 14:06:16 +0200

    Terminado Iban

---
+++ ENDE/IbanNS/IbanNS/Iban.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IbanNS
+{
+	public class ParametrosIncorrectos : ArgumentException { };
+	public class LongitudIncorrecta : ArgumentOutOfRangeException { };
+
+	public class Iban
+	{
+		// ES7100302053091234567895
+
+		public string IBN { get; set; }
+		
+		public bool EsIbanValido(string IBN)
+		{
+			return IBN == CalcularIBAN(IBN.Substring(4));
+		}
+
+		public int GetLength()
+		{
+			return IBN.Length;
+		}
+
+		private string CalcularIBAN(string cc)
+		{
+			if (!ComprobarLongitudCC(ref cc))
+				throw new LongitudIncorrecta();
+			if (!ComprobarPatronIban(cc))
+				throw new ParametrosIncorrectos();
+
+			// ES00 España
+			string ES = cc + "142800";
+
+			string[] partesCC = new string[5];
+			TrocearCC(ES, ref partesCC);
+
+			int iresultado = 0;
+			string resultado = partesCC[0];
+
+			CalcularModulo(partesCC, ref iresultado, ref resultado);
+
+			int iRestoIban = 98 - int.Parse(resultado);
+			string restoIban = iRestoIban.ToString();
+			if (restoIban.Length == 1)
+				restoIban = "0" + restoIban;
+
+			return "ES" + restoIban + cc;
+		}
+
+		private void CalcularModulo(string[] partesCC, ref int iresultado, ref string resultado)
+		{
+			for (int i = 0; i < partesCC.Length - 1; i++)
+			{
+				iresultado = int.Parse(resultado + partesCC[i + 1]) % 97;
+				resultado = iresultado.ToString();
+			}
+		}
+
+		private void TrocearCC(string cc, ref string[] partesCC)
+		{
+			partesCC[0] = cc.Substring(0, 5);
+			partesCC[1] = cc.Substring(5, 5);
+			partesCC[2] = cc.Substring(10, 5);
+			partesCC[3] = cc.Substring(15, 5);
+			partesCC[4] = cc.Substring(20, 6);
+		}
+
+		private bool ComprobarLongitudCC(ref string cc)
+		{
+			for (int i = 0; i < cc.Length; i++)
+			{
+				if (cc[i] == ' ')
+					cc = cc.Remove(i, 1);
+			}
+			if (cc.Length != 20)
+				return false;
+			return true;
+		}
+		private bool ComprobarPatronIban(string cc)
+		{
+			string patron = @"\A[A-Z]{2}[0-9]{22}\Z";
+			Regex reg = new Regex(patron);
+
+			return reg.IsMatch(cc);
+		}
+
+	}
+}
---
+++ ENDE/IbanNS/IbanNSTest/IbanTest.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using IbanNS;
+
+namespace IbanNSTest
+{
+	[TestFixture]
+    public class IbanTest
+    {
+		private string V = "ES7100302053091234567895";
+		private Iban _iban;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_iban = new Iban();
+		}
+		[Test]
+		public void LongitudIbanCorrecto()
+		{
+			_iban.IBN = V;
+			int LongitudMax = 24;
+
+			Assert.AreEqual(LongitudMax, _iban.GetLength());
+		}
+		[Test]
+		public void ValorAntesDePasarIban()
+		{
+			Assert.AreEqual(null, _iban.IBN);
+		}
+		[Test]
+		public void ComprobarIbanCorrecto()
+		{
+			Assert.IsTrue(_iban.EsIbanValido(V));
+		}
+		[Test]
+		public void ComprobarIbanEspañaCorrecto()
+		{
+			_iban.IBN = V;
+
+			Assert.AreEqual("ES", _iban.IBN.Substring(0,2));
+		}
+		[Test]
+		public void LongitudIncorrecta()
+		{
+			try
+			{
+				_iban.EsIbanValido("ES710030203353091234567895");
+				Assert.Fail("Falla porque supera la longitud permitida");
+			}
+			catch(LongitudIncorrecta)
+			{
+				// Funciona
+			}
+		}
+		[Test]
+		public void ParametrosIncorrectos()
+		{
+			try
+			{
+				_iban.EsIbanValido("ESAH51328745236541258525");
+				Assert.Fail("Falla porque no se paso un iban");
+			}
+			catch(ParametrosIncorrectos)
+			{
+				// Funciona
+			}
+		}
+    }
+}
