5598486dd1c484dee657507df74debe39b5b7d92
Douglas Jurado Peña <douglaswarner.jp@gmail.com>
2019-06-05 12:44:30 +0200

    Prestamos Bancos

---
+++ Programacion_C#/Tema_9_WPF/Prestamos_Bancos/Banco.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestamos_Bancos
+{
+    class Prestamo
+    {
+        private const double INTERES = 1.10;
+        private const double CARGOEXTRA = 1.01;
+        private double _cantidad;
+        private int _meses;
+        private bool _esRiesgo;
+
+        public bool EsRiesgo
+        {
+            get { return _esRiesgo; }
+            set { _esRiesgo = value; }
+        }
+        public int Meses
+        {
+            get { return _meses; }
+            set
+            {
+                if (value < 25 && value > 0)
+                    _meses = value;
+                else
+                    _meses = 0;
+            }
+        }
+        public double Cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; }
+        }
+
+        public Prestamo()
+        { }
+
+        public Prestamo(double cantidad, int meses)
+        {
+            Cantidad = cantidad;
+            Meses = meses;
+            EsRiesgo = false;
+        }
+
+        public double CalcularCantidadMensual()
+        {
+            if (EsRiesgo)
+            {
+                return ((Cantidad * CARGOEXTRA) * INTERES) / Meses;
+            }
+            else
+            {
+                return (Cantidad * INTERES) / Meses;
+            }
+        }
+    }
+}
---
+++ Programacion_C#/Tema_9_WPF/Prestamos_Bancos/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prestamos_Bancos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Prestamo cliente = new Prestamo();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularCuota_Click(object sender, RoutedEventArgs e)
+        {
+            double tmpCantidad = 0;
+            int tmpMeses = 0;
+            try
+            {
+                if (double.TryParse(tbxCantidad.Text, out tmpCantidad))
+                    cliente.Cantidad = tmpCantidad;
+                else
+                {
+                    MessageBox.Show("Error: Algo con la cantidad");
+                    return;
+                }
+                if (int.TryParse(tbxMeses.Text, out tmpMeses))
+                    cliente.Meses = int.Parse(tbxMeses.Text);
+                else
+                {
+                    MessageBox.Show("Error: Algo con los meses");
+                    return;
+                }
+
+                if (cbxRiesgo.IsChecked == true)
+                    cliente.EsRiesgo = true;
+                else
+                    cliente.EsRiesgo = false;
+
+                tbkCuotaMensual.Text = Math.Round(cliente.CalcularCantidadMensual(),4).ToString() + " €";
+            }
+            catch
+            {
+                MessageBox.Show("Error: algo ocurrio con la conversion");
+            }
+        }
+
+        private void tbxMeses_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            string[] meses = {"1","2","3","4" };   
+
+            if(meses.Contains(e.Text))
+                e.Handled = true;
+        }
+    }
+}
